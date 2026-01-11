77a13d847f95f56b882b4d2d33db5090706165b6
Shubham Rai <shubhamrai6523@gmail.com>
2026-01-11 23:24:20 +0530

    first commit

---
+++ My.py
@@ -0,0 +1,15 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> for i in range(1,6)
+SyntaxError: expected ':'
+>>> for i in range(1,6):
+...     for j in range(i,0):
+...         print("1",end=" ")
+...     print()
+... 
+...     
+
+
+
+
+
